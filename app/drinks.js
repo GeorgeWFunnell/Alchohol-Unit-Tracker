7a5f57bede58b728178e479aa91d5c5070a08ccb
UP2013395 <91505266+UP2013395@users.noreply.github.com>
2023-08-31 14:07:04 +0100

    database stuff (BROKEN)

--- app/drinks.js
+++ app/drinks.js
@@ -1,21 +1,21 @@
 import { loadErrorPage } from './template_loading.js';
 
 export async function addDrinkInfo() {
-    // Gets the information required to make a record
-    const drinkName = document.querySelector("#inputBox").value;
-    const quantity = document.querySelector("#quantity").value;
-    let drink = await fetch("find/" + drinkName);
-    drink = await drink.json();
-    // Checks the drink exists and that the user inputted a quantity
-    if (drink === undefined || quantity < 1 || quantity === undefined) {
-        loadErrorPage("ErrorP");
-    }
-    // adjusts unit count depending on size of shot and adds the drink
-    if (document.querySelector("#Single").checked) {
-        await fetch("add/" + drink.ID + "/" + quantity + "/" + drink.Price);
-    } else if (document.querySelector("#Double").checked) {
-        await fetch("add/" + drink.ID + "/" + quantity * 2 + "/" + drink.Price * 2);
-    } else {
-        loadErrorPage("ErrorP");
-    }
+  // Gets the information required to make a record
+  const drinkName = document.querySelector('#inputBox').value;
+  const quantity = document.querySelector('#quantity').value;
+  let drink = await fetch('find/' + drinkName);
+  drink = await drink.json();
+  // Checks the drink exists and that the user inputted a quantity
+  if (drink === undefined || quantity < 1 || quantity === undefined) {
+    loadErrorPage('ErrorP');
+  }
+  // adjusts unit count depending on size of shot and adds the drink
+  if (document.querySelector('#Single').checked) {
+    await fetch('add/' + drink.ID + '/' + quantity + '/' + drink.Price);
+  } else if (document.querySelector('#Double').checked) {
+    await fetch('add/' + drink.ID + '/' + quantity * 2 + '/' + drink.Price * 2);
+  } else {
+    loadErrorPage('ErrorP');
+  }
 }
--- app/template_loading.js
+++ app/template_loading.js
@@ -1,104 +1,185 @@
-import { addDrinkInfo } from './drinks.js'
+import { addDrinkInfo } from './drinks.js';
+import { combineDrinkNameID } from './database.js';
 
 // Sets up event listeners for the hamburger menu which only needs to be done once
 function setUp() {
-    loadMainMenu();
-    document.querySelector("#hamburger").addEventListener("click", loadHamburger);
-    document.querySelector("#Home").addEventListener("click", loadMainMenu);
-    document.querySelector("#TrackedUnits").addEventListener("click", loadTrackingPage);
-    document.querySelector("#InputDrinks").addEventListener("click", loadEditMenu);
-    document.querySelector("#DrinkDatabase").addEventListener("click", loadDatabasePage);
+  loadMainMenu();
+  document.querySelector('#hamburger').addEventListener('click', loadHamburger);
+  document.querySelector('#Home').addEventListener('click', loadMainMenu);
+  document.querySelector('#TrackedUnits').addEventListener('click', loadTrackingPage);
+  document.querySelector('#InputDrinks').addEventListener('click', loadEditMenu);
+  document.querySelector('#DrinkDatabase').addEventListener('click', loadDatabasePage);
 }
 
 // Clears the pageArea in index.html to load another template
 function clearBody(area) {
-    const pageArea = document.querySelector(area);
-    while (pageArea.hasChildNodes()) {
-        pageArea.removeChild(pageArea.firstChild);
-    }
+  const pageArea = document.querySelector(area);
+  while (pageArea.hasChildNodes()) {
+    pageArea.removeChild(pageArea.firstChild);
+  }
 }
 
 // Loads the desired template onto the page
 function loadPage(pageID) {
-    const newPage = document.querySelector(`#${pageID}`);
-    clearBody("#pageArea");
-    const newContent = newPage.content.cloneNode(true);
-    document.querySelector("#pageArea").appendChild(newContent);
+  const newPage = document.querySelector(`#${pageID}`);
+  clearBody('#pageArea');
+  const newContent = newPage.content.cloneNode(true);
+  document.querySelector('#pageArea').appendChild(newContent);
 }
 
 // Opens or Closes the hamburger menu
 function loadHamburger() {
-    const menu = document.querySelector("#MyLinks");
-    if (menu.classList.contains("inactive")) {
-        menu.classList.remove("inactive");
-    } else {
-        menu.classList.add("inactive");
-    }
+  const menu = document.querySelector('#MyLinks');
+  if (menu.classList.contains('inactive')) {
+    menu.classList.remove('inactive');
+  } else {
+    menu.classList.add('inactive');
+  }
 }
 
-// Prints the database onto the page
+/* Prints the database onto the page
 async function showDatabaseInfo() {
-    clearBody(".informationArea");
-    const div = document.querySelector(".informationArea");
-    let database = await fetch("fulldatabase");
-    database = await database.json();
-    for (let drink of database) {
-        const drinkInfo = document.createElement("p");
-        drinkInfo.textContent = JSON.stringify(drink);
-        div.append(drinkInfo);
-    }
+  clearBody('.informationArea');
+  const div = document.querySelector('.informationArea');
+  let database = await fetch('fulldatabase');
+  database = await database.json();
+  for (const drink of database) {
+    const drinkInfo = document.createElement('p');
+    drinkInfo.className = 'showTables';
+    drinkInfo.textContent = JSON.stringify(drink);
+    div.append(drinkInfo);
+  }
+}
+*/
+
+async function showDatabaseInfo() {
+  clearBody('.informationArea');
+  const div = document.querySelector('.informationArea');
+  let database = await fetch('fulldatabase');
+  database = await database.json();
+  for (const drink of database) {
+    const drinkInfo = createDrinkInfoElementDatabase(drink);
+    div.appendChild(drinkInfo);
+  }
 }
 
-// Prints drink history onto the page
+/*
+ Prints drink history onto the page
 async function showDrinkHistory() {
-    clearBody(".informationArea");
-    const div = document.querySelector(".informationArea");
-    let history = await fetch("fullrecord");
-    history = await history.json();
-    for (let drink of history) {
-        const drinkInfo = document.createElement("p");
-        drinkInfo.textContent = JSON.stringify(drink);
-        div.append(drinkInfo);
+  clearBody('.informationArea');
+  const div = document.querySelector('.informationArea');
+  let history = await fetch('fullrecord');
+  history = await history.json();
+  for (const drink of history) {
+    const drinkInfo = document.createElement('p');
+    drinkInfo.className = 'showTables';
+    drinkInfo.textContent = JSON.stringify(drink);
+    div.append(drinkInfo);
+  }
+} */
+
+async function showDrinkHistory() {
+  try {
+    clearBody('.informationArea');
+
+    const div = document.querySelector('.informationArea');
+    const response = await fetch('fullrecord');
+    const history = await response.json();
+
+    for (const drink of history) {
+      const drinkInfo = createDrinkInfoElement(drink);
+      div.appendChild(drinkInfo);
     }
+  } catch (error) {
+    console.error('Error fetching drink history:', error);
+  }
 }
+
+async function createDrinkInfoElement(drink) {
+  const drinkInfo = document.createElement('div');
+  drinkInfo.classList.add('drinkInfo');
+
+  const drinkData = await combineDrinkNameID(); // Call the function to get the data
+  const matchingDrink = drinkData.find(item => item.ID === drink.Drink_ID);
+
+  if (matchingDrink) {
+    const nameLabel = document.createElement('p');
+    nameLabel.textContent = `Name: ${matchingDrink.DrinkName}`;
+    drinkInfo.appendChild(nameLabel);
+  }
+
+  const typeLabel = document.createElement('p');
+  typeLabel.textContent = `Type: ${drink.type}`;
+  drinkInfo.appendChild(typeLabel);
+
+  const timeLabel = document.createElement('p');
+  timeLabel.textContent = `Time: ${drink.TIME}`;
+  drinkInfo.appendChild(timeLabel);
+
+  const unitLabel = document.createElement('p');
+  unitLabel.textContent = `Units: ${drink.Units}`;
+  drinkInfo.appendChild(unitLabel);
+
+  // Add more fields as needed
+
+  return drinkInfo;
+}
+
+function createDrinkInfoElementDatabase(drink) {
+  const drinkInfo = document.createElement('div');
+  drinkInfo.classList.add('drinkInfo');
+
+  const nameLabel = document.createElement('p');
+  nameLabel.textContent = `Name: ${drink.name}`;
+  drinkInfo.appendChild(nameLabel);
+
+  const typeLabel = document.createElement('p');
+  typeLabel.textContent = `Type: ${drink.type}`;
+  drinkInfo.appendChild(typeLabel);
+
+  // Add more fields as needed
+
+  return drinkInfo;
+}
+
 
 // Updates values on main page and configures buttons
 function loadMainMenu() {
-    loadPage("MenuP");
-    document.querySelector("#AUnit").textContent = "46";
-    document.querySelector("#BALevel").textContent = "688%";
-    document.querySelector("#Price").textContent = "£11.70";
-    document.querySelector("#editB").addEventListener("click", loadEditMenu);
-    document.querySelector("#moreB").addEventListener("click", loadDatabasePage);
-    document.querySelector("#awareB").addEventListener("click", loadDrinkAware);
+  loadPage('MenuP');
+  document.querySelector('#AUnit').textContent = '46';
+  document.querySelector('#BALevel').textContent = '688%';
+  document.querySelector('#Price').textContent = '£11.70';
+  document.querySelector('#editB').addEventListener('click', loadEditMenu);
+  document.querySelector('#moreB').addEventListener('click', loadDatabasePage);
+  document.querySelector('#awareB').addEventListener('click', loadDrinkAware);
 }
 
 // Displays drink history
 async function loadTrackingPage() {
-    loadPage("TrackP");
-    await showDrinkHistory();
+  loadPage('TrackP');
+  await showDrinkHistory();
 }
 
 // Loads the menu to allow adding to the drink record
 function loadEditMenu() {
-    loadPage("EditDrinkP");
-    document.querySelector("#buttonA").addEventListener("click", addDrinkInfo);
+  loadPage('EditDrinkP');
+  document.querySelector('#buttonA').addEventListener('click', addDrinkInfo);
 }
 
 // Displays the full Drink Database
 async function loadDatabasePage() {
-    loadPage("DatabaseP");
-    await showDatabaseInfo();
+  loadPage('DatabaseP');
+  await showDatabaseInfo();
 }
 
 // Sends user to the drink aware website
 function loadDrinkAware() {
-    window.location.assign("https://www.drinkaware.co.uk/")
+  window.location.assign('https://www.drinkaware.co.uk/');
 }
 
 // Basic error page that can be called by other functions if some error occurs
 export function loadErrorPage() {
-    loadPage("ErrorP");
+  loadPage('ErrorP');
 }
 
-window.addEventListener("load", setUp);
+window.addEventListener('load', setUp);
